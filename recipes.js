f603500008cc622b892694fc529b56a247987157
Charles <charleszecevic@gmail.com>
2023-02-16 23:09:26 +0100

    aled

--- recipes.js
+++ recipes.js
@@ -1779,19 +1779,20 @@ const recipes = [
 // ingredients
 
 const ingredientList = document.querySelector("#ingredientsList");
+const ingredientsInput = document.querySelector("#ingredientsInput");
 const btnIngredient = document.querySelector("#ingredientsButton");
 
 // appareil
 const appareilList = document.querySelector("#AppareilList");
+const appareilInput = document.querySelector("#AppareilInput");
 const btnAppareil = document.querySelector("#AppareilButton");
 
 // ustensiles
 const ustensilesList = document.querySelector("#UstensilesList");
+const ustensilesInput = document.querySelector("#UstensilesInput");
 const btnUstensiles = document.querySelector("#UstensilesButton");
 
 // plat
-
-
 
 recipes.forEach((recipe) => {
   btnIngredient.addEventListener("click", () => {
@@ -1803,6 +1804,10 @@ recipes.forEach((recipe) => {
 
   btnAppareil.addEventListener("click", () => {
     // console.log(recipe.appliance);
+    let appreilArray = [];
+    appareilInput.addEventListener("keypress", () => {
+      console.log(appareilInput.value);
+    });
     appareilList.innerHTML += "<li>" + recipe.appliance + "</li>";
   });
 
