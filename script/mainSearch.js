0a6d46dda88835de893b1097f7caeb955bf2f4e0
Charles <charleszecevic@gmail.com>
2023-04-06 19:48:12 +0200

    correct mainSearch

--- script/mainSearch.js
+++ script/mainSearch.js
@@ -5,17 +5,35 @@ let currentRecipes = [...recipes];
 
 const mainSearch = (recipes) => {
   searchInput.addEventListener("keyup", (e) => {
-    let keypressed = e.target.value.toLowerCase();
+    let keypressed = e.target.value;
+    currentRecipes = [];
     if (keypressed.length > 2) {
-      currentRecipes = recipes.filter((recipe) => {
-        const nameDescription =
-          recipe.name.toLowerCase().includes(keypressed) ||
-          recipe.description.toLowerCase().includes(keypressed);
-        const ingredient = recipe.ingredients.some((ingredient) =>
-          ingredient.ingredient.toLowerCase().includes(keypressed)
-        );
-        return nameDescription || ingredient;
-      });
+      for (let i = 0; i < recipes.length; i++) {
+        let nameDescription = false;
+        let ingredient = false;
+        if (
+          recipes[i].name.toLowerCase().includes(keypressed.toLowerCase()) ||
+          recipes[i].description
+            .toLowerCase()
+            .includes(keypressed.toLowerCase())
+        ) {
+          nameDescription = true;
+        }
+        recipes[i].ingredients.forEach((ingredient) => {
+          // console.log(ingredient.ingredient);
+          if (
+            ingredient.ingredient
+              .toLowerCase()
+              .includes(keypressed.toLowerCase())
+          ) {
+            ingredient = true;
+          }
+        });
+        if (nameDescription || ingredient)
+          if (!currentRecipes.includes(recipes[i])) {
+            currentRecipes.push(recipes[i]);
+          }
+      }
       displayRecipes(currentRecipes);
     } else {
       displayRecipes(recipes);
@@ -26,41 +44,3 @@ const mainSearch = (recipes) => {
 export { mainSearch, currentRecipes };
 
 // utiliser filter a la place de foreach
-
-// const mainSearch = (recipes) => {
-//   searchInput.addEventListener("keyup", (e) => {
-//     let keypressed = e.target.value;
-//     currentRecipes = [];
-//     if (keypressed.length > 2) {
-//       for (let i = 0; i < recipes.length; i++) {
-//         let nameDescription = false;
-//         let ingredient = false;
-//         if (
-//           recipes[i].name.toLowerCase().includes(keypressed.toLowerCase()) ||
-//           recipes[i].description
-//             .toLowerCase()
-//             .includes(keypressed.toLowerCase())
-//         ) {
-//           nameDescription = true;
-//         }
-//         recipes[i].ingredients.forEach((ingredient) => {
-//           // console.log(ingredient.ingredient);
-//           if (
-//             ingredient.ingredient
-//               .toLowerCase()
-//               .includes(keypressed.toLowerCase())
-//           ) {
-//             ingredient = true;
-//           }
-//         });
-//         if (nameDescription || ingredient)
-//           if (!currentRecipes.includes(recipes[i])) {
-//             currentRecipes.push(recipes[i]);
-//           }
-//       }
-//       displayRecipes(currentRecipes);
-//     } else {
-//       displayRecipes(recipes);
-//     }
-//   });
-// };
